3c783c4f6c0ea5f2a9946e3b50b5e08a73d06a39
Josh Ullman <joshullman@gmail.com>
2015-09-24 16:43:16 -0700

    Add Cell Class

--- connect_four.rb
+++ connect_four.rb
@@ -3,4 +3,15 @@ class Board
 		@board_size = board_size
 		@win_size = win_size
 	end
+
+	def create_cells
+		
+	end
+end
+
+class Cell
+	def initialize(x_position, y_position)
+		@x_position = x_position
+		@y_position = y_position
+	end
 end
