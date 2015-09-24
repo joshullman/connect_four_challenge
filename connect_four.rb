182737b9476f91f1b7cbf83528aa4979c0e34aba
Josh Ullman <joshullman@gmail.com>
2015-09-24 16:54:17 -0700

    Add create-cells method to board

--- connect_four.rb
+++ connect_four.rb
@@ -1,17 +1,38 @@
 class Board
+	attr_reader :board
 	def initialize(board_size, win_size)
 		@board_size = board_size
 		@win_size = win_size
+		@board = []
 	end
 
 	def create_cells
-		
+		y_position = 1
+		@board_size.times do
+			x_position = "A"
+			row = []
+			@board_size.times do
+				row << Cell.new(x_position, y_position)
+				x_position = x_position.next
+			end
+			@board << row
+			y_position += 1
+		end
 	end
 end
 
 class Cell
+	attr_accessor :x_position, :y_position
 	def initialize(x_position, y_position)
 		@x_position = x_position
 		@y_position = y_position
 	end
 end
+
+board = Board.new(7, 4)
+board.create_cells
+board.board.each do |row|
+	p row
+	puts
+end
+
