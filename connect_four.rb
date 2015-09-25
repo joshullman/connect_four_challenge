0cc86ba0d748384d7a2c4b0fb11476cd386162cb
Josh Ullman <joshullman@gmail.com>
2015-09-24 17:07:32 -0700

    Add player class, basic win-checker architecture

--- connect_four.rb
+++ connect_four.rb
@@ -1,8 +1,8 @@
 class Board
 	attr_reader :board
-	def initialize(board_size, win_size)
-		@board_size = board_size
-		@win_size = win_size
+	def initialize(params = {})
+		@board_size = params[:board_size]
+		@win_size = params[:win_size]
 		@board = []
 	end
 
@@ -12,24 +12,63 @@ class Board
 			x_position = "A"
 			row = []
 			@board_size.times do
-				row << Cell.new(x_position, y_position)
+				row << Cell.new({x_position: x_position, y_position: y_position})
 				x_position = x_position.next
 			end
 			@board << row
 			y_position += 1
 		end
 	end
-end
 
-class Cell
-	attr_accessor :x_position, :y_position
-	def initialize(x_position, y_position)
-		@x_position = x_position
-		@y_position = y_position
+	def determine_first_move
+		coin_toss = rand(2)
+	end
+
+	def player_move(position, player)
+
+	end
+
+	def check_for_wins
+		@win = false
+	end
+
+	def check_horiz_wins(cell)
+		counter = 0
+	end
+
+	def check_vert_wins(cell)
+		counter = 0
+	end
+
+	def check_diag_left_wins(cell)
+		counter = 0
+	end
+
+	def check_diag_right_wins(cell)
+		counter = 0
 	end
 end
 
-board = Board.new(7, 4)
+class Cell
+	attr_accessor :x_position, :y_position, :value
+	def initialize(params = {})
+		@x_position = params[:x_position]
+		@y_position = params[:y_position]
+		@value = []
+	end
+end
+
+class Player
+	attr_reader :name, :symbol
+	def initialize(params = {})
+		@name = params[:name]
+		@symbol = params[:symbol]
+	end
+end
+
+# player_one = Player.new({name: , symbol: })
+# player_two = Player.new({name: , symbol: })
+board = Board.new({board_size: 7, win_size: 4})
 board.create_cells
 board.board.each do |row|
 	p row
